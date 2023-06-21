47dccb451aac0f1c641bc6cec94f7bc36dba43cd
Kirill_Glushankov <kiglu7@gmail.com>
2023-06-21 18:34:54 +0500

    add homework #1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.fixture
+def test_product():
+     return Item("ТВ", 15000, 2)
+
+def test_calculate_total_price(test_product):
+    assert test_product.calculate_total_price() == 30000
+
+def test_apply_discount(test_product):
+    Item.pay_rate = 0.8
+    test_product.apply_discount()
+    assert test_product.price == 12000
